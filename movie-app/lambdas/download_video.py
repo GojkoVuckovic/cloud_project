92880965758d1fc396457cdf8fe09ea9e279f6c6
Arezinko <arezinamilann@gmail.com>
2024-09-05 20:10:08 +0200

    Add base code video transcoding and dynamo db metadata

--- movie-app/lambdas/download_video.py
+++ movie-app/lambdas/download_video.py
@@ -1,37 +1,33 @@
+import boto3
 import os
 import base64
-import boto3
-import json
+
+s3_client = boto3.client('s3')
+bucket_name = os.environ['BUCKET_NAME']
 
 def handler(event, context):
-    s3_client = boto3.client('s3')
-    bucket_name = os.environ['BUCKET_NAME']
-    
-    # Extract the file name from query string parameters
-    file_name = event['queryStringParameters'].get('file-name', '')
-    format = event['queryStringParameters'].get('format', 'original')  # Default to 'original' if no format is specified
-
-    # Generate the key for the specific format
-    key = f'{format}/{file_name}' if format != 'original' else file_name
+    # Extract the file name from the event
+    file_name = event['queryStringParameters']['file-name']
 
     try:
-        # Retrieve the video object from S3
-        response = s3_client.get_object(Bucket=bucket_name, Key=key)
-        video_data = response['Body'].read()
-        
-        # Encode binary data to base64
-        base64_encoded_video = base64.b64encode(video_data).decode('utf-8')
+        # Get the file from S3
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        file_content = response['Body'].read()
+
+        # Base64 encode the binary data
+        base64_encoded_content = base64.b64encode(file_content).decode('utf-8')
 
         return {
             'statusCode': 200,
-            'body': base64_encoded_video,
-            'isBase64Encoded': True,  # Important for binary data
+            'body': base64_encoded_content,
+            'isBase64Encoded': True,
             'headers': {
-                'Content-Type': 'video/mp4'  # Adjust if your video is in a different format
+                'Content-Type': 'video/mp4',
+                'Content-Disposition': f'attachment; filename="{file_name}"'
             }
         }
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': f'Error: {str(e)}'
         }
