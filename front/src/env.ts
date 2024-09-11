8b328ad92e572b123d35ef8c8bfe483716d6023d
Vuk <vuk.dimitrov@gmail.com>
2024-09-12 00:06:53 +0200

    Enviroment changes

--- front/src/env.ts
+++ front/src/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiGateway: 'https://rs41y1helk.execute-api.eu-central-1.amazonaws.com/prod/',
-  UserPoolId: 'eu-central-1_yO1oYieJS',
-  ClientId: '6h12674dcc5c0p0q92erfegt4t'
+  apiGateway: 'https://f6k9diz1xl.execute-api.eu-central-1.amazonaws.com/prod/',
+  UserPoolId: 'eu-central-1_9Jrn2tUtT',
+  ClientId: 'vhpovvno9j8fup8qn7btjvth3'
 }
